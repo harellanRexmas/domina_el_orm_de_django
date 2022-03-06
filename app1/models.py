4d2bff120e6112fc1eff36c2327cd9929d7ec731
Harinson Arellan <harellan@manager.cl>
2022-03-06 20:27:26 -0300

    modulo 1: finalizado

---
+++ app1/models.py
@@ -0,0 +1,16 @@
+from tabnanny import verbose
+from django.db import models
+
+# Create your models here.
+class Categoria(models.Model):
+    descripcion = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.descripcion
+    
+    def save(self):
+        self.descripcion = self.descripcion.upper()
+        super(Categoria, self).save()
+    
+    class Meta:
+        verbose_name_plural = "Categorias"
